0369779a4eb44e3c1d3ccdd56922e924deba56bc
steveburton4 <Steven.burton@hotmail.co.uk>
2021-06-11 16:25:37 +0100

    TF13 (#24)
    * TF13

--- terraform/modules/networking/provider.tf
+++ terraform/modules/networking/provider.tf
@@ -2,7 +2,7 @@ provider "aws" {
   alias = "cluster-broker"
 
   region  = var.region
-  version = "~> 2.62.0"
+  version = "~> 3.42.0"
 
   assume_role {
     role_arn = var.role_arn.cluster-broker
