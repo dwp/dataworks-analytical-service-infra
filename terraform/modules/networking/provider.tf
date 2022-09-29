9a5d32eca99fd5dd5795f4be9b1a8844ab185e89
Jonathan Melia <jonathan.melia@engineering.digital.dwp.gov.uk>
2022-09-29 13:18:54 +0100

    Removed provider

--- terraform/modules/networking/provider.tf
+++ terraform/modules/networking/provider.tf
@@ -1,10 +1,16 @@
-provider "aws" {
+/* provider "aws" {
   alias = "cluster-broker"
 
   region  = var.region
-  version = "~> 3.42.0"
 
   assume_role {
     role_arn = var.role_arn.cluster-broker
   }
-}
+} */
+/* 
+terraform {
+  required_providers {
+    aws = {
+      version = "~> 3.42.0"
+    }
+} */
