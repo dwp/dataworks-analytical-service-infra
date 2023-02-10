5d3173ce0153b592ae31818dca6da1f17c283490
Jonathan Melia <jonathan.melia@engineering.digital.dwp.gov.uk>
2023-02-10 09:19:48 +0000

    AWS Provider 3.76.1 and tagging

--- terraform/deploy/infra/locals.tf
+++ terraform/deploy/infra/locals.tf
@@ -24,4 +24,16 @@ locals {
   dw_domain = "${local.env_prefix[local.environment]}dataworks.dwp.gov.uk"
 
   kali_users = jsondecode(data.aws_secretsmanager_secret_version.internet_ingress.secret_binary)["ssh_bastion_users"]
+
+  # Configured as per Tagging doc requirements https://engineering.dwp.gov.uk/policies/hcs-cloud-hosting-policies/resource-identification-tagging/
+  # Also required as per Tenable documentation https://engineering.dwp.gov.uk/products/gold-images/agents/tenable/
+  hcs_environment = {
+    development    = "Dev"
+    qa             = "Test"
+    integration    = "Stage"
+    preprod        = "Stage"
+    production     = "Production"
+    management     = "SP_Tooling"
+    management-dev = "DT_Tooling"
+  }
 }
--- terraform/modules/networking/locals.tf
+++ terraform/modules/networking/locals.tf
@@ -1,4 +1,16 @@
 locals {
   zone_count = length(data.aws_availability_zones.current.zone_ids)
   zone_names = data.aws_availability_zones.current.names
+
+  # Configured as per Tagging doc requirements https://engineering.dwp.gov.uk/policies/hcs-cloud-hosting-policies/resource-identification-tagging/
+  # Also required as per Tenable documentation https://engineering.dwp.gov.uk/products/gold-images/agents/tenable/
+  hcs_environment = {
+    development    = "Dev"
+    qa             = "Test"
+    integration    = "Stage"
+    preprod        = "Stage"
+    production     = "Production"
+    management     = "SP_Tooling"
+    management-dev = "DT_Tooling"
+  }
 }
